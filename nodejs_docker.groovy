d8dd14d31d006a0af4becca357cd8a02e50d60fe
epsilonradius2@gmail.com <epsilonradius2@gmail.com>
2019-08-30 16:00:40 -0700

    added file

---
+++ nodejs_docker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS Docker example1') {
+    scm {
+        git('https://github.com/epsilonradius/Docker-NodeJS-app.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('epsilonradius@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('epsilonradius/Jenkins_DSL')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
